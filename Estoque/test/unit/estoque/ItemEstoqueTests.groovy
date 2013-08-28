bf51e0ebdf29fb689b7a3e6b325357eb24a2e362
OpenShift guest <ale182juve@msn.com>
2013-08-28 19:55:29 -0400

    test

---
+++ Estoque/test/unit/estoque/ItemEstoqueTests.groovy
@@ -0,0 +1,17 @@
+package estoque
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(ItemEstoque)
+class ItemEstoqueTests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}
